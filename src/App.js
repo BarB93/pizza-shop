2328be2d78798a3bc9700af9333655b84fe6e3dc
bbarovsky <barovskiyboris@gmail.com>
2022-07-16 22:40:59 +0300

    first setting up: deleted junk files, added some components and  scss styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Categories from './components/Categories'
+import Header from './components/Header'
+import PizzaItem from './components/PizzaItem'
+import Sort from './components/Sort'
+import './scss/app.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaItem title={'Чизбургер-пицца'} price={395} />
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
