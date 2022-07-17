508e2e126d90a78a1665e3c16cc75add2d4dcb5a
bbarovsky <barovskiyboris@gmail.com>
2022-07-17 08:34:32 +0300

    PizzaItem component: change layout and styles

--- src/components/PizzaItem.js
+++ src/components/PizzaItem.js
@@ -1,38 +1,45 @@
 import React, { useState } from 'react'
 
-const PizzaItem = ({ title, price, imageUrl, sizes, types }) => {
+const PizzaItem = ({ title, price, imageUrl, sizes, types, description }) => {
   const [activeType, setActiveType] = useState(types[0])
   const [activeSize, setActiveSize] = useState(0)
+  const [activePizza, setActivePizza] = useState(false)
   const pizzaTypes = ['тонкое', 'традиционное']
+  const pricesForSize = [0, 280, 420]
 
   return (
-    <div className='pizza-block'>
-      <img className='pizza-block__image' src={imageUrl} alt={title} />
-      <h4 className='pizza-block__title'>{title}</h4>
-      <div className='pizza-block__selector'>
-        <ul>
-          {types.map((typeId) => (
-            <li key={typeId} className={activeType === typeId ? 'active' : ''} onClick={() => setActiveType(typeId)}>
-              {pizzaTypes[typeId]}
-            </li>
-          ))}
-        </ul>
-        <ul>
-          {sizes.map((s, index) => (
-            <li key={s} className={activeSize === index ? 'active' : ''} onClick={() => setActiveSize(index)}>
-              {s} см.
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className='pizza-block__bottom'>
-        <div className='pizza-block__price'>от {price} ₽</div>
-        <div className='button button--outline button--add'>
-          <svg width='12' height='12' viewBox='0 0 12 12' fill='none' xmlns='http://www.w3.org/2000/svg'>
-            <path d='M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z' fill='white' />
-          </svg>
-          <span>Добавить</span>
-          <i>2</i>
+    <div className='wrapper-pizza-block'>
+      <div className={`pizza-block ${activePizza ? 'active' : ''}`}>
+        <img className='pizza-block__image' src={imageUrl} alt={title} />
+        <h4 className='pizza-block__title'>{title}</h4>
+        <div className='pizza-block__description'>{description}</div>
+        <div className='pizza-block__bottom'>
+          <button className='pizza-block__btn-select' onClick={() => setActivePizza(true)}>Выбрать</button>
+          <span className={'pizza-block__price'}>от {price} ₽</span>
+        </div>
+        <div className='pizza-block__selector selector'>
+          <h4 className='pizza-block__title'>{title}</h4>
+          <div className='pizza-block__description'>{description}</div>
+          <ul className='selector__sizes'>
+            {sizes.map((s, index) => (
+              <li key={s} className={`selector__size ${activeSize === index ? 'active' : ''}`} onClick={() => setActiveSize(index)}>
+                {s} см.
+              </li>
+            ))}
+          </ul>
+          <ul className='selector__types'>
+            {types.map((typeId) => (
+              <li key={typeId} className={`selector__type ${activeType === typeId ? 'active' : ''}`} onClick={() => setActiveType(typeId)}>
+                {pizzaTypes[typeId]}
+              </li>
+            ))}
+          </ul>
+          <div className='selector__bottom'>
+            <button className='selector__btn'>
+              <span>В корзину</span>
+              <span>{price + pricesForSize[activeSize]} ₽</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
