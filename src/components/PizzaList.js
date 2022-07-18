fbdf91b69c93069c8ab3287a321ffcd38b6cb51e
bbarovsky <barovskiyboris@gmail.com>
2022-07-18 07:30:44 +0300

    created skeleton for PizzaItem

--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-import PizzaItem from './PizzaItem'
-
-import pizzas from '../assets/pizzas.json'
+import PizzaItem from './PizzaItem/PizzaItem'
+import PizzaItemSkeleton from './PizzaItem/PizzaItemSkeleton'
 
 const PizzaList = () => {
+  const [pizzas, setPizzas] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    fetch('https://62d45a565112e98e484e62f8.mockapi.io/pizzas')
+      .then((data) => data.json())
+      .then((pizzas) => {
+        setPizzas(pizzas)
+        setIsLoading(false)
+      })
+      .catch((e) => console.error('Error: ' + e.message))
+  }, [])
+
   return (
     <>
-      <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {pizzas.map((pizza) => (
-          <PizzaItem key={pizza.title} {...pizza} />
-        ))}
-      </div>
+      <h2 className='content__title'>Все пиццы</h2>
+      <div className='content__items'>{isLoading ? new Array(12).fill(0).map(item =><PizzaItemSkeleton /> ) : pizzas.map((pizza) => <PizzaItem key={pizza.title} {...pizza} />)}</div>
     </>
   )
 }
