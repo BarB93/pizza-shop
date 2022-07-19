f7dc1f6b616ec359d81c0317df884553b7264b9f
BarB93 <barovskiyboris@gmail.com>
2022-07-19 04:28:04 +0300

    PizzaItemSkeleton component: deleted lib, made own skeleton

--- src/components/PizzaItem/PizzaItemSkeleton.js
+++ src/components/PizzaItem/PizzaItemSkeleton.js
@@ -1,15 +1,20 @@
 import React from 'react'
-import ContentLoader from 'react-content-loader'
 
 const PizzaItemSkeleton = (props) => {
   return (
     <div className='wrapper-pizza-block'>
-      <ContentLoader className='pizza-block' speed={2} width={280} height={391} viewBox='0 0 280 391' backgroundColor='#f3f3f3' foregroundColor='#ecebeb' {...props}>
-        <rect x='0' y='264' rx='5' ry='5' width='280' height='60' />
-        <rect x='0' y='352' rx='22' ry='22' width='124' height='40' />
-        <rect x='201' y='362' rx='5' ry='5' width='74' height='27' />
-        <circle cx='136' cy='122' r='120' />
-      </ContentLoader>
+      <div className='pizza-block'>
+        <div className='pizza-block-skeleton__image-wrapper'>
+          <div className='pizza-block-skeleton__image skeleton'></div>
+        </div>
+        <div className='pizza-block-skeleton__info skeleton'></div>
+        <div className='pizza-block-skeleton__footer'>
+          <div  className='pizza-block-skeleton__bottom'>
+              <div className='pizza-block-skeleton__btn skeleton'></div>
+              <div className='pizza-block-skeleton__price skeleton'></div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -20,7 +20,7 @@ const PizzaList = () => {
   return (
     <>
       <h2 className='content__title'>Все пиццы</h2>
-      <div className='content__items'>{isLoading ? new Array(12).fill(0).map(item =><PizzaItemSkeleton /> ) : pizzas.map((pizza) => <PizzaItem key={pizza.title} {...pizza} />)}</div>
+      <div className='content__items'>{isLoading ? new Array(12).fill(0).map((item, index) =><PizzaItemSkeleton key={index}/> ) : pizzas.map((pizza) => <PizzaItem key={pizza.title} {...pizza} />)}</div>
     </>
   )
 }
