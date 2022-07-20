26f56a61d88efe400ff6ef1447157760e5a275f0
BarB93 <barovskiyboris@gmail.com>
2022-07-20 07:16:00 +0300

    added swiper lib and use it in Categories component

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -1,24 +1,26 @@
 import React, { useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
+import 'swiper/css'
+import 'swiper/css/pagination'
 import styles from './Categories.module.scss'
 
 const Categories = () => {
-    const [activeCategory, setActiveCategory] = useState(0)
+  const [activeCategory, setActiveCategory] = useState(4)
 
-    const categories = [
-        'Все',
-        'Мясные',
-        'Вегетарианская',
-        'Гриль',
-        'Острые',
-        'C грибами'
-    ]
-    
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'C грибами']
+
   return (
     <div className={styles.categories}>
-      <ul>
-        {categories.map((category, index) => <li key={category} className={index === activeCategory ? styles.active : ''} onClick={() => setActiveCategory(index)}>{category}</li>)}
-      </ul>
+      <div className={styles.list}>
+        <Swiper slidesPerView='auto' initialSlide={activeCategory}>
+          {categories.map((category, index) => (
+            <SwiperSlide key={category} className={`${styles.slide} ${index === activeCategory ? styles.active : ''}`} onClick={() => setActiveCategory(index)}>
+              {category}
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   )
 }
