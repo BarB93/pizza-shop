c82de597472966e7eafb55d4f52634b7dce84b1f
BarB93 <barovskiyboris@gmail.com>
2022-07-21 11:53:12 +0300

    created CartList component

---
+++ src/components/CartList/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CartItem from '../CartItem'
+
+const CartList = () => {
+  return (
+    <>
+      <CartItem />
+      <CartItem />
+      <CartItem />
+      <CartItem />
+    </>
+  )
+}
+
+export default CartList
