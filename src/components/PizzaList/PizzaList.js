dbc7a0c2b8c2fb4c97881bcaafe26ef9feae9b1c
BarB93 <barovskiyboris@gmail.com>
2022-07-21 12:43:54 +0300

    PizzaList component: added window scroll

--- src/components/PizzaList/PizzaList.js
+++ src/components/PizzaList/PizzaList.js
@@ -11,18 +11,20 @@ const PizzaList = () => {
 
   useEffect(() => {
     fetch('https://62d45a565112e98e484e62f8.mockapi.io/pizzas')
-      .then((data) => data.json())
-      .then((pizzas) => {
+      .then(data => data.json())
+      .then(pizzas => {
         setPizzas(pizzas)
         setIsLoading(false)
       })
-      .catch((e) => console.error('Error: ' + e.message))
+      .catch(e => console.error('Error: ' + e.message))
+
+    window.scrollTo(0, 0)
   }, [])
 
   return (
     <>
       <h2 className={styles.title}>Все пиццы</h2>
-      <div className={styles.list}>{isLoading ? new Array(8).fill(0).map((item, index) => <PizzaItemSkeleton key={index} />) : pizzas.map((pizza) => <PizzaItem key={pizza.title} {...pizza} />)}</div>
+      <div className={styles.list}>{isLoading ? new Array(8).fill(0).map((item, index) => <PizzaItemSkeleton key={index} />) : pizzas.map(pizza => <PizzaItem key={pizza.title} {...pizza} />)}</div>
     </>
   )
 }
