116a8959e1a02cd23b76245ec55a63fc1e0b6211
BarB93 <barovskiyboris@gmail.com>
2022-07-22 18:02:12 +0300

    Cart compoenent: removed unused imports

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,18 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+
 import CartBottom from '../../components/CartBottom'
 import CartList from '../../components/CartList'
 import CartTop from '../../components/CartTop'
-import Button from '../../components/UI/Controls/Button'
 
 import styles from './Cart.module.scss'
 
 const Cart = () => {
   return (
     <div className={styles.container}>
-        <CartTop />
-        <CartList />
-        <CartBottom />
+      <CartTop />
+      <CartList />
+      <CartBottom />
     </div>
   )
 }
