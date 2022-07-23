9c2de64d8593bb1aafbef49f359a4e08df07773b
BarB93 <barovskiyboris@gmail.com>
2022-07-23 05:15:18 +0300

    create utils/consts.js, added sortItems

---
+++ src/utils/consts.js
@@ -0,0 +1,9 @@
+export const sortItems = [
+  { value: { name: 'rating', order: 'desc' }, label: 'популярности max' },
+  { value: { name: 'rating', order: 'asc' }, label: 'популярности min' },  
+  { value: { name: 'price', order: 'desc' }, label: 'цене max' },
+  { value: { name: 'price', order: 'asc' }, label: 'цене min' },
+  { value: { name: 'title', order: 'asc' }, label: 'алфавиту А-Я' },
+  { value: { name: 'title', order: 'desc' }, label: 'алфавиту Я-А' },
+  { value: null, label: 'без сортировки' },
+]
