20ef69c2f5c444fd87fd4ff6affa0458312cc7a8
BarB93 <barovskiyboris@gmail.com>
2022-07-23 11:15:10 +0300

    Categories component: use redux store

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -1,14 +1,16 @@
-import React, { useContext } from 'react'
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { Swiper, SwiperSlide } from 'swiper/react'
 
-import { Context } from '../../App'
+import {setCategoryId} from '../../store/slices/categorySlice'
 
 import 'swiper/css'
 import 'swiper/css/pagination'
 import styles from './Categories.module.scss'
 
 const Categories = () => {
-  const { categoryId, setCategoryId } = useContext(Context)
+  const categoryId = useSelector(state => state.category.categoryId)
+  const dispatch = useDispatch()
 
   const categories = ['Все', 'Мясные', 'Вегетарианские', 'Гриль', 'Острые', 'C грибами']
 
@@ -17,7 +19,7 @@ const Categories = () => {
       <div className={styles.list}>
         <Swiper slidesPerView='auto' initialSlide={categoryId}>
           {categories.map((category, index) => (
-            <SwiperSlide key={category} className={`${styles.slide} ${index === categoryId ? styles.active : ''}`} onClick={() => setCategoryId(index)}>
+            <SwiperSlide key={category} className={`${styles.slide} ${index === categoryId ? styles.active : ''}`} onClick={() => dispatch(setCategoryId(index))}>
               {category}
             </SwiperSlide>
           ))}
