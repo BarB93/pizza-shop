5a78d1ad32e47a3eade377ca7a4ed44f495f4fde
BarB93 <barovskiyboris@gmail.com>
2022-07-25 00:50:00 +0300

    Search component: fixed callback onClick close icon

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -20,7 +20,7 @@ const Search = () => {
         <line fill='none' id='XMLID_44_' stroke='#000000' strokeLinecap='round' strokeLinejoin='round' strokeMiterlimit='10' strokeWidth='2' x1='27' x2='20.366' y1='27' y2='20.366'></line>
       </svg>
       <input className={styles.input} type='text' value={search} onChange={searchChangeHandler} placeholder='Поиск пиццы...' />
-      {search && <svg className={styles.closeIcon} onClick={() => {setSearch('')}} viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/></svg>}
+      {search && <svg className={styles.closeIcon} onClick={() => dispatch(setSearch(''))} viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/></svg>}
     </div>
   )
 }
