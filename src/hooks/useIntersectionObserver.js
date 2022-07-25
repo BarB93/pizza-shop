a8cad8a6a56e9c50a8ebfd80b47a4d0ffbdc825f
BarB93 <barovskiyboris@gmail.com>
2022-07-25 16:50:50 +0300

    created custom hook useIntersectionObserver

---
+++ src/hooks/useIntersectionObserver.js
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from 'react'
+
+export const useIntersectionObserver = ({ ref, canLoad, isLoading, currentPage, callback }) => {
+  const observer = useRef()
+
+  useEffect(() => {
+    if (isLoading) return
+    if (observer.current) observer.current.disconnect()
+
+    const options = {
+      root: document,
+      rootMargin: '0px',
+      threshold: 0,
+    }
+
+    const cb = (entries, observer) => {
+      if (entries[0].isIntersecting && canLoad) {
+        callback()
+      }
+    }
+
+    observer.current = new IntersectionObserver(cb, options)
+    observer.current.observe(ref.current)
+    //eslint-disable-next-line
+  }, [isLoading, currentPage])
+}
