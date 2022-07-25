053acc2be85c364c1e7ff8acb9c843e7ddfb2091
BarB93 <barovskiyboris@gmail.com>
2022-07-25 16:51:38 +0300

    pizzaAPI: added params limit and currentPage

--- src/api/pizzaAPI.js
+++ src/api/pizzaAPI.js
@@ -5,12 +5,16 @@ const pizzaAPI = {
     const category = params?.category
     const sort = params?.sort
     const search = params?.search
+    const limit = params?.limit
+    const currentPage = params?.currentPage
 
     const { data } = await $host.get('/', {
       params: {
         ...(search ? { title: search } : {}),
         ...(category ? { category: params.category } : {}),
         ...(sort ? { sortBy: params.sort.name, order: params.sort.order } : {}),
+        ...(limit ? { limit } : {}),
+        ...(currentPage ? { page: currentPage } : {}),
       },
     })
     return data
