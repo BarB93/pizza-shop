0069e9ed8ab956e86573891efec22dbccb5eb2bf
BarB93 <barovskiyboris@gmail.com>
2022-07-28 14:14:37 +0300

    Button component: added styles

--- src/components/UI/Controls/Button/index.js
+++ src/components/UI/Controls/Button/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import clsx from 'clsx'
 
 import styles from './Button.module.scss'
 
 const Button = ({type, children, className, onClick}) => {
-  let classes = `${styles.button} `
-  if(type === 'select') classes += styles.select
-  if(className) classes += className
+  const classes = clsx(
+    styles.button, 
+    type === 'select' && styles.select,
+    type === 'transparent' && styles.transparent,
+    type === 'black' && styles.black,
+    className && className
+  )
+  
   return (
     <button className={classes} onClick={onClick}>
       {children}
