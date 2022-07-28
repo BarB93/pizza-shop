efd93ea6c01dcae383db9f1e1af2147bd93c4ac5
BarB93 <barovskiyboris@gmail.com>
2022-07-28 14:16:04 +0300

    NotFound component: use Button component, add styles

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import Button from '../../components/UI/Controls/Button'
+
 import styles from './NotFound.module.scss'
 
 const NotFound = () => {
@@ -9,7 +11,7 @@ const NotFound = () => {
       <h1 className={styles.title}>{'Страница не найдена :('}</h1>
       <p>К сожеления данная страница отсутвует в нашем интернет-магазине</p>
       <Link to="/">
-        <button className={styles.btn}>На главную</button>
+        <Button className={styles.btn} type='black'>На главную</Button>
       </Link>
     </div>
   )
