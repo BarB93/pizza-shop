85a9e03abc7aecdd8d81be07072a59266add5a38
BarB93 <barovskiyboris@gmail.com>
2022-07-28 14:20:39 +0300

    CircleControl component: added styles for disabled state

--- src/components/UI/Controls/CircleControl/index.js
+++ src/components/UI/Controls/CircleControl/index.js
@@ -3,9 +3,9 @@ import clsx from 'clsx'
 
 import styles from './CircleControl.module.scss'
 
-const CalcControl = ({ minus, plus, remove, onClick }) => {
+const CalcControl = ({ minus, plus, remove, onClick, disabled}) => {
   return (
-    <button className={clsx(styles.button, minus && styles.button_minus, remove && styles.button_remove)} onClick={onClick}>
+    <button className={clsx(styles.button, minus && styles.button_minus, remove && styles.button_remove)} disabled={disabled} onClick={onClick}>
       {(minus || plus) && (
         <svg width='10' height='10' viewBox='0 0 10 10' fill='none' xmlns='http://www.w3.org/2000/svg'>
           <path d='M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z' fill='#EB5A1E' />
